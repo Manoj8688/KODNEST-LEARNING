671bec1669ee06b4ce51891290a2993a10c7dbaf
Manoj Tapala <tmanoj.scsvmv@gmail.com>
2023-08-20 21:21:12 +0530

    ASSIGNMENTS

--- Journeycalculator/src/journeyCalculate/JourneycalculatorApp.java
+++
@@ -1,9 +0,0 @@
-package journeyCalculate;
-
-public class JourneycalculatorApp {
-	public double calculateDistance(double speed, double time)
-	{
-		return speed*time;
-	}
-
-}
--- TheTemperatureConverter/src/tempconverter/temperatureConverter.java
+++
@@ -1,9 +0,0 @@
-package tempconverter;
-
-public class temperatureConverter {
-	public double convertFahrenheitToCelsius(double F)
-	{
-		return (F - 32) * 5/9;
-	}
-
-}
